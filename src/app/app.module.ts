4a97ef40d3e222555ead09170e04a65a72e810a5
alfredo fernandez <afernandez8781@hotmail.com>
2018-01-10 17:57:36 -0400

    Input, output y Viewchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { APP_ROUTES } from './app.routes';
 //Modulos
 import { PagesModule } from './pages/pages.module';
 
+//Temporal
+import { FormsModule } from '@angular/forms';
 
 //Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
 
 
 @NgModule({
@@ -23,7 +26,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
 	BrowserModule,
 	APP_ROUTES,
-	PagesModule
+	PagesModule,
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
