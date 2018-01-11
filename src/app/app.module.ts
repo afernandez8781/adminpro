86adb75a647bf087aed861322a1907a6f5727ce4
alfredo fernandez <afernandez8781@hotmail.com>
2018-01-11 16:35:25 -0400

    Fin de la sección 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,14 @@ import { PagesModule } from './pages/pages.module';
 //Temporal
 import { FormsModule } from '@angular/forms';
 
+//Servicios
+import { ServiceModule } from './services/service.module';
+
 //Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
 
 
 
@@ -27,9 +31,10 @@ import { RegisterComponent } from './login/register.component';
 	BrowserModule,
 	APP_ROUTES,
 	PagesModule,
-	FormsModule
+	FormsModule,
+	ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
