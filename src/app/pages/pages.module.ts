9ba461ee4436a0f3d569a210c6cd45ca9226438f
alfredo fernandez <afernandez8781@hotmail.com>
2018-01-21 16:53:45 -0400

    Sección 14 lista

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { PAGES_ROUTES } from './pages.routes';
 import { SharedModule } from '../shared/shared.module';
 
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 // ng2-charts
@@ -16,6 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+//Pipe Module
+import { PipesModule } from '../pipes/pipes.module';
 
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
@@ -23,6 +26,7 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
 import { AccoutSettingsComponent } from './accout-settings/accout-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     declarations: [
@@ -34,7 +38,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         GraficoDonaComponent,
         AccoutSettingsComponent,
         PromesasComponent,
-        RxjsComponent
+        RxjsComponent,
+        ProfileComponent
     ],
     exports: [
         DashboardComponent,
@@ -42,10 +47,12 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         Graficas1Component
     ],
     imports: [
+		CommonModule,
         SharedModule,
         PAGES_ROUTES,
         FormsModule,
-        ChartsModule
+		ChartsModule,
+		PipesModule
     ]
 })
 export class PagesModule { }
